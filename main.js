37d42ed6437c895719023148d8dc4cf3f2fc200b
vm4music <vm4music@gmail.com>
2023-01-27 12:38:29 +0800

    test

--- main.js
+++ main.js
@@ -88,6 +88,44 @@ app.post('/searchSongs', async function (req, res) {
 
 });
 
+app.post('/play', (req, res) => {
+
+  console.log(req.body.name)
+
+  const path = 'mymusicapp/songs/' + req.body.name + '.aac';
+  
+  var stat = fs.statSync(path);
+  var total = stat.size;
+
+  var range = req.headers.range;
+  if (!range) range = 'bytes=0-'
+  
+  console.log(range + ' is the range... and total: ' + total);
+
+
+  if (range != '') {
+    
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total - 1;
+    var chunksize = (end - start) + 1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+    var file = fs.createReadStream(path, { start: start, end: end });
+    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'audio/mpeg' });
+    file.pipe(res);
+
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
+  }
+
+
+})
+
 app.get('/play/:id', async (req, res) => {
 
 
@@ -98,6 +136,8 @@ app.get('/play/:id', async (req, res) => {
   var stat = fs.statSync(path);
   var total = stat.size;
 
+  var range = req.headers.range;
+  if (!range) range = 'bytes=0-'
 
   
   // try {
